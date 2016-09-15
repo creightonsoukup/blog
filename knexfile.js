ec6e3087600c7794867afd8eefad8e8c72acb329
Your Name <creighton@creightonsoukup.com>
2016-09-14 19:14:33 -0600

    initial commit

---
+++ knexfile.js
@@ -0,0 +1,26 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'pq',
+    connection: 'postgres://localhost/blog'
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
